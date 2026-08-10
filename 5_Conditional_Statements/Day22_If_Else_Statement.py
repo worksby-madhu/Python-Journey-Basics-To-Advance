bba813e7eccc42170ba559016e898219be1ed696
Madhu Lakshmi Subhadra VILLA <lakshmisubhadravilla@gmail.com>
2026-08-10 17:43:19 +0530

    Add files via upload

---
+++ 5_Conditional_Statements/Day22_If_Else_Statement.py
@@ -0,0 +1,32 @@
+# Example 1: Check Even or Odd
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(num, "is an Even Number")
+else:
+    print(num, "is an Odd Number")
+
+
+# Example 2: Check Positive or Negative
+num = int(input("Enter another number: "))
+if num > 0:
+    print(num, "is a Positive Number")
+else:
+    print(num, "is a Negative Number")
+
+
+# Example 3: Pass or Fail
+marks = int(input("Enter your marks: "))
+if marks >= 35:
+    print("Pass")
+else:
+    print("Fail")
+
+
+# Example 4: Voting Eligibility
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("Eligible to Vote")
+else:
+    print("Not Eligible to Vote")
+
+
