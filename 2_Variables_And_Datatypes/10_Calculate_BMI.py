966d8593cf8701126a13b03abec280a4b9b12ff7
Madhu Lakshmi Subhadra VILLA <lakshmisubhadravilla@gmail.com>
2026-09-07 16:11:21 +0530

    BMI calculation and round function

--- 2_Variables_And_Datatypes/10_Calculate_BMI.py
+++ 2_Variables_And_Datatypes/10_Calculate_BMI.py
@@ -9,4 +9,4 @@ print("ur BMI is:",bmi) #17.224803717352987
 print("ur BMI is:",round(bmi)) #17
 """round() function is used to make numbers shorter and cleaner by limiting decimal places"""
 print("ur BMI is:",round(bmi,5)) #17.2248
-"""round(number, digits) here limiting to 5"""
+"""round(number, digits) here limiting to 5""" 
