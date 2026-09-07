4c77c9a7f837d257162000be26974c9d43e6d8ce
Madhu Lakshmi Subhadra VILLA <lakshmisubhadravilla@gmail.com>
2026-09-07 16:08:23 +0530

    Coding exercise - Add digits of a string using typecasting

--- 2_Variables_And_Datatypes/09_Add_Digits_Of_Num_CE.py
+++ 2_Variables_And_Datatypes/09_Add_Digits_Of_Num_CE.py
@@ -4,4 +4,4 @@ num_2= num[1]
 ''' accessing'''
 print(num_1+num_2) #47
 '''num_1 and num_2 are strings so we need to use type conversion here to add the digits'''
-print(int(num_1)+int(num_2)) #11 
+print(int(num_1)+int(num_2)) #11
