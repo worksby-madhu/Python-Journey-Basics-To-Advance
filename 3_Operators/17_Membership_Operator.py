1f7bb24bbafbebae5ad90944e68b19a5dc14527e
Madhu Lakshmi Subhadra VILLA <lakshmisubhadravilla@gmail.com>
2026-09-08 22:43:19 +0530

    Membership operator examples with strings and lists

--- 3_Operators/17_Membership_Operator.py
+++ 3_Operators/17_Membership_Operator.py
@@ -10,7 +10,7 @@ list=[1,2,3,-1,-2,-3,00]
 print(1 in [1,2,3,-1,-2,-3,00])
 print(-1 in list )
 print(-1 in (-2,-3,0,-1))
-print(0 in list)       #in python 0 and 00 are same
+print(0 in list)      #in python 0 and 00 are same
 print( 00 in list)
 print(0 not in list)
 print(2-3 in list)
