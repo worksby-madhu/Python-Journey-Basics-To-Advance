edc89edd55e55279f92ae577fddd0d380a3d4222
Madhu Lakshmi Subhadra VILLA <lakshmisubhadravilla@gmail.com>
2026-09-11 09:50:29 +0530

    Add basic if statement examples

--- 5_Conditional_Statements/21_If_Statement.py
+++ 5_Conditional_Statements/21_If_Statement.py
@@ -4,20 +4,17 @@ if num % 2 == 0:
     print(num, "is an Even Number")
 print("--------------------")
 
-
 # Example 2: Check Positive Number
 num = int(input("Enter another number: "))
 if num > 0:
     print(num, "is a Positive Number")
 print("--------------------")
 
-
 # Example 3: Check Divisible by 5
 num = int(input("Enter another number: "))
 if num % 5 == 0:
     print(num, "is Divisible by 5")
 print("--------------------")
-
 
 # Example 4: Voting Eligibility
 age = int(input("Enter your age: "))
